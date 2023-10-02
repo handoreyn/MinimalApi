8d1e085d2622e2993809c453fdb6621f723ea282
Fatih Gencaslan <fatih.gencaslan@adesso.com.tr>
2023-10-02 23:20:19 +0300

    User class constructor properties updated

--- src/Riot.Minimal.Api/Persistence/Entities/User.cs
+++ src/Riot.Minimal.Api/Persistence/Entities/User.cs
@@ -5,10 +5,15 @@ public sealed class User
     public int Id { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
+    public string Password { get; set; }
     public StatusEnumType Status { get; set; }
 
-    public User(int id, string username, string email, StatusEnumType status = StatusEnumType.Active)
+    public User(int id, string username, string email, string password, StatusEnumType status = StatusEnumType.Active)
     {
-
+        Id = id;
+        Username = username;
+        Email = email;
+        Password = password;
+        Status = status;
     }
 }
